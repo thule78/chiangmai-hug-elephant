f5fb62b1ea94df8c42f8343c199f8aeabea46533
thule <lunthu@gmail.com>
2019-05-17 19:34:33 +0700

    fixed models-booking syntax end keyword error

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,9 +3,8 @@ class Booking < ApplicationRecord
   belongs_to :customer, class_name: "User", foreign_key: 'customer_id'
   validates :headcount, presence: true, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 15}
   validates :date, presence: true
-end
 
-def self.has_unconfirmed_requests?(user)
+  def self.has_unconfirmed_requests?(user)
     unconfirmed_bookings = 0
     user.tours.each do |tour|
       tour.bookings.select do |booking|
