d38439db459069ab8b60e386689f8e3960861cd6
thule <lunthu@gmail.com>
2019-05-22 18:41:41 +0700

    add email column to booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_090839) do
+ActiveRecord::Schema.define(version: 2019_05_22_112804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_05_21_090839) do
     t.datetime "updated_at", null: false
     t.string "pick_up"
     t.text "message"
+    t.string "email"
     t.index ["tour_id"], name: "index_bookings_on_tour_id"
   end
 
